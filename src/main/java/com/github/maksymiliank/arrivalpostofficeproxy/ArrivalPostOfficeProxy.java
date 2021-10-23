ec85a960a290a8d7ec25cb93c7f3f8d3c46cca60
MaksymilianK <kondziugeniusz@gmail.com>
2021-10-23 22:36:48 +0200

    Init project

---
+++ src/main/java/com/github/maksymiliank/arrivalpostofficeproxy/ArrivalPostOfficeProxy.java
@@ -0,0 +1,11 @@
+package com.github.maksymiliank.arrivalpostofficeproxy;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class ArrivalPostOfficeProxy extends Plugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
